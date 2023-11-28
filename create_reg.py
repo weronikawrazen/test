52a3cdf570f0821ef844377a5de211e84f7ad786
weronikawrazen <weronikawrazen@gmail.com>
2023-11-28 15:18:37 +0100

    komenatarz

---
+++ create_reg.py
@@ -0,0 +1,6 @@
+class Feature:
+    def __init__(self, name, distribution, min_avg, max_var):
+        self.name = name
+        self.distribution = distribution
+        self.min_avg = min_avg
+        self.max_var = max_var
